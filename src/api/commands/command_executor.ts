6f567c73ce1ed63b14d6a11cea39aba3755a812f
PokeTrainerLena <98965392+PokeTrainerLena@users.noreply.github.com>
2022-03-21 16:35:20 +0100

    imortet messages

--- src/api/commands/command_executor.ts
+++ src/api/commands/command_executor.ts
@@ -4,10 +4,12 @@ import { ChatClient, PrivateMessage } from '@twurple/chat';
 export abstract class CommandExecutor implements Command {
     private _name: string;
     private _alias: string[];
+    private _description: string;
 
-    constructor(name: string, alias: string[]) {
+    constructor(name: string, alias: string[],description: string) {
         this._name = name;
         this._alias = alias;
+        this._description = description;
     }
     abstract execute(command: string, channel: string, msg: PrivateMessage, apiClient: ApiClient, chatClient: ChatClient, args: string[]): boolean;
 
@@ -18,6 +20,9 @@ export abstract class CommandExecutor implements Command {
 
     get alias(): string[] {
         return this._alias;
+    }
+    get description(): string {
+        return this._description;
     }
 }
 
--- src/commands/social_command.ts
+++ src/commands/social_command.ts
@@ -1,16 +1,47 @@
-
 import { ApiClient } from "@twurple/api";
 import { ChatClient, PrivateMessage } from '@twurple/chat';
 import { CommandExecutor } from "../api/commands/command_executor";
+import { socials } from "../messages/socials.json";
 
 export class SocialCommand extends CommandExecutor {
 
     constructor() {
-        super("dc", ["yt", "dc"]);
+        super("dc", ['discord', 'dc', 'donate', 'tip', 'doni', 'spende', 'freundescode', 'fc', 'mitspielen', 'instagram', 'ig', 'insta', 'youtube', 'yt'], "Damit gebe ich dir einen social-Link von Leni zurück");
     }
 
     execute(command: string, channel: string, sender: PrivateMessage, apiClient: ApiClient, chatClient: ChatClient, args: string[]): boolean {
-        chatClient.say(channel, "Jo fuck you self :D", { replyTo: sender.id });
+        switch (command) {
+            case "discord":
+            case "dc":
+                var nachricht = socials["discord"];
+                break;
+            case "donate":
+            case "tip":
+            case "doni":
+            case "spende":
+                var nachricht = socials["donate"];
+                break;
+            case "freundescode":
+            case "fc":
+            case "mitspielen":
+                var nachricht = socials["freundescode"];
+                break;
+            case "instagram":
+            case "ig":
+            case "insta":
+                var nachricht = socials["instagram"];
+                break;
+            case "youtube":
+            case "yt":
+                var nachricht = socials["youtube"];
+                break;
+            default:
+                var nachricht=[""];
+                break;
+        }
+
+        chatClient.say(channel, nachricht[0], { replyTo: sender.id });
+        sender.userInfo.userType
         return true;
     }
 
