2ae1d30fe36a086eaa85b604b78f8ddf5de1da54
PokeTrainerLena <98965392+PokeTrainerLena@users.noreply.github.com>
2022-03-21 01:54:32 +0100

    Add catch

--- src/listeners/follwers/follow_listener.ts
+++ src/listeners/follwers/follow_listener.ts
@@ -14,8 +14,12 @@ export class FollowListener implements Listener<EventSubChannelFollowEvent> {
 
     on(event: EventSubChannelFollowEvent, apiClient: ApiClient, chatClient: ChatClient):void {
         event.getBroadcaster().then(r => {
-            chatClient.say(r.name, "Danke für dein follow @" + event.userDisplayName).then();
-        })
+            chatClient.say(r.name, "Danke für dein follow @" + event.userDisplayName).then().catch(reason => {
+                console.log(reason);
+            });
+        }).catch(reason => {
+            console.log(reason);
+        });
         
 
     }
